6a61eed2882e01000d8ff98cba9c50c335c7a800
Simone Colabufalo <simone.colabufalo@gmail.com>
2021-04-06 08:57:39 +0200

    Tailwind JIT feature example on Base layout

--- src/layouts/Base.vue
+++ src/layouts/Base.vue
@@ -2,7 +2,7 @@
   <div
     class="w-full min-h-screen text-gray-900 transition duration-100 bg-gray-50 dark:bg-gray-900 dark:text-gray-50"
   >
-    <div class="flex items-center p-6 space-x-5">
+    <div class="flex items-center p-[30px] space-x-5">
       <div>Imagine this is a great Header</div>
       <button
         class="px-3 py-2 font-bold leading-none transition bg-pink-500 rounded-lg dark:bg-indigo-500 focus:outline-none dark:hover:bg-indigo-900 hover:bg-pink-900"
@@ -26,13 +26,8 @@
       name: "BaseLayout",
     },
     setup() {
-      // const dark = ref(false);
       const isDark = useDark();
       const toggleDark = useToggle(isDark);
-      // const toggleDark = () => {
-      //   document.documentElement.classList.toggle("dark");
-      // };
-
       return { toggleDark };
     },
   };
