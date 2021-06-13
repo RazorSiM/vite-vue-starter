2b8645af89f14dcb7e054fcfdc285caeb1946e56
Raz <me@raz.works>
2021-06-13 23:05:48 +0200

    formatted code

--- src/layouts/Base.vue
+++ src/layouts/Base.vue
@@ -1,11 +1,31 @@
 <template>
   <div
-    class="w-full min-h-screen text-gray-900 transition duration-100 bg-gray-50 dark:bg-gray-900 dark:text-gray-50"
+    class="
+      w-full
+      min-h-screen
+      text-gray-900
+      transition
+      duration-100
+      bg-gray-50
+      dark:bg-gray-900 dark:text-gray-50
+    "
   >
     <div class="flex items-center p-[30px] space-x-5">
       <div>Imagine this is a great Header</div>
       <button
-        class="px-3 py-2 font-bold leading-none transition bg-pink-500 rounded-lg dark:bg-indigo-500 focus:outline-none dark:hover:bg-indigo-900 hover:bg-pink-900"
+        class="
+          px-3
+          py-2
+          font-bold
+          leading-none
+          transition
+          bg-pink-500
+          rounded-lg
+          dark:bg-indigo-500
+          focus:outline-none
+          dark:hover:bg-indigo-900
+          hover:bg-pink-900
+        "
         @click="toggleDark"
       >
         If you click here you toggle the dark mode
