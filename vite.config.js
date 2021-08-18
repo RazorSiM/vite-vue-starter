96d85db4d24995d3a5236fd698ef53b615ef0e99
Raz <me@raz.works>
2021-08-18 17:55:45 +0200

    enabled ref sugar

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
+import WindiCSS from "vite-plugin-windicss";
 import { defineConfig } from "vite";
 import path from "path";
 import vue from "@vitejs/plugin-vue";
-import WindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS()],
+  plugins: [
+    vue({
+      script: {
+        refSugar: true,
+      },
+    }),
+    WindiCSS(),
+  ],
   resolve: {
     alias: {
       "/@": path.resolve(__dirname, "/src"),
